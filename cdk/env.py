865b5a375faa1ab5662f3bb235e63d684733c904
Jakub Zeman <jakub.zeman@scala.ai>
2025-07-31 13:34:06 +0200

    CDK synth fixed

--- cdk/env.py
+++ cdk/env.py
@@ -1,28 +1,30 @@
-import os
-
 ENVIRONMENTS = {
     "playground": {
-        "vpcId": os.environ.get("PLAYGROUND_VPC_ID", "vpc-12345678"),
-        "subnetIds": os.environ.get("PLAYGROUND_SUBNET_IDS", "subnet-12345678,subnet-87654321").split(","),
-        "domain": os.environ.get("PLAYGROUND_DOMAIN", "playground.example.com"),
+        "vpcId": "vpc-09a336bc51ebc097e",
+        "subnetIds": [
+            "subnet-0933b0bb314dcdc60",
+            "subnet-07e07050f56ac0920",
+            "subnet-09069cfc18f7c22fe",
+        ],
+        "domain": "flutter-hello-world.playground.scala.ai",
         "cpu": 256,
-        "memory": 512,
+        "memory": 1024,
         "AutoScaleMinCapacity": 1,
-        "AutoScaleMaxCapacity": 2,
+        "AutoScaleMaxCapacity": 3,
     },
     "preview": {
-        "vpcId": os.environ.get("PREVIEW_VPC_ID", "vpc-12345678"),
-        "subnetIds": os.environ.get("PREVIEW_SUBNET_IDS", "subnet-12345678,subnet-87654321").split(","),
-        "domain": os.environ.get("PREVIEW_DOMAIN", "preview.example.com"),
+        "vpcId": "",
+        "subnetIds": [],
+        "domain": "flutter-hello-world.preview.scala.technology",
         "cpu": 512,
         "memory": 1024,
         "AutoScaleMinCapacity": 1,
         "AutoScaleMaxCapacity": 3,
     },
     "live": {
-        "vpcId": os.environ.get("LIVE_VPC_ID", "vpc-12345678"),
-        "subnetIds": os.environ.get("LIVE_SUBNET_IDS", "subnet-12345678,subnet-87654321").split(","),
-        "domain": os.environ.get("LIVE_DOMAIN", "live.example.com"),
+        "vpcId": "",
+        "subnetIds": [],
+        "domain": "flutter-hello-world.scala.ai",
         "cpu": 1024,
         "memory": 2048,
         "AutoScaleMinCapacity": 2,
