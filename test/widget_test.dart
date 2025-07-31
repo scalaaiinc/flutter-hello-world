fce22203a3b97160684dc5af3ad62b8103e86ee9
Jakub Zeman <jakub.zeman@scala.ai>
2025-07-31 12:15:23 +0200

    UNIT Tests fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_hello_world/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Hello World app smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our app displays the correct text.
+    expect(find.text('Hello, World!'), findsOneWidget);
+    expect(find.text('Welcome to Flutter on AWS Fargate'), findsOneWidget);
+    expect(find.text('Flutter Hello World'), findsOneWidget); // AppBar title
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the Flutter Dash icon is present.
+    expect(find.byIcon(Icons.flutter_dash), findsOneWidget);
   });
 }
